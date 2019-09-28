832b6c520e7abdb0816bae816d209f2d68313b44
Howard Yang <howardyang25@gmail.com>
2019-09-27 19:01:10 -0700

    Set up webpack and server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Modulo is listening on port ${port}!`);
+});
+
+app.use(morgan('dev'));
+
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
